2f8aa35ea516eaef319a98770dbd975efdab7ec6
zoujunqi <55213549+zoujunqi@users.noreply.github.com>
2021-09-16 22:43:27 +0800

    Update multi_proxy_client.py

--- multi_proxy_client.py
+++ multi_proxy_client.py
@@ -32,7 +32,7 @@ def main():
     address = [(HOST, PORT)]
     #establish 10 different connections
     with Pool() as p:
-        p.map(connect, address * 3)
+        p.map(connect, address * 10)
 
 if __name__ == "__main__":
     main()
